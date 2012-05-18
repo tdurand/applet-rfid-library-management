0ecc1c10b7b9122efbb18023ff82b09ddbcd5c21
Thibault DURAND <tibo.durand@gmail.com>
2012-05-18 18:08:09 -0300

    WIP: Applet with both side communication

---
+++ signjar.sh
@@ -0,0 +1,1 @@
+jarsigner -keystore key -storepass password -keypass password -signedjar applet-library-management-signed.jar applet-library-management.jar signFiles
--- src/rfid/RFIDAccess.java
+++ src/rfid/RFIDAccess.java
@@ -5,12 +5,19 @@ import java.awt.Graphics;
 import java.io.IOException;
 import java.util.Properties;
 
+import netscape.javascript.JSException;
+import netscape.javascript.JSObject;
+
 import br.com.rfidcoe.yarf.device.RFIDReaderFactory;
 import br.com.rfidcoe.yarf.device.reader.RFIDReader;
 
 public class RFIDAccess extends Applet {
     
-    public String identification;
+    public String toDraw;
+    public Properties readerProperties;
+    public RFIDReader rfidReader;
+    private JSObject jso;
+    public String idRead;
 
     @Override
     public void init() {
@@ -19,33 +26,25 @@ public class RFIDAccess extends Applet {
 
     @Override
     public void start() {
-        Properties readerProperties=new Properties();
         
+        readerProperties=new Properties();
         readerProperties.setProperty("yarf.device.model", "MERCURY5");
         readerProperties.setProperty("reader.power", "3000");
-        readerProperties.setProperty("yarf.device.host", "10.102.10.1");
+        readerProperties.setProperty("yarf.device.host", "10.102.10.88");
         
+        rfidReader = new RFIDReaderFactory().createReader(readerProperties);
         
-        RFIDReader rfidReader = new RFIDReaderFactory().createReader(readerProperties);
+        connect();
+        
+        toDraw="initdone";
         
         try {
-            rfidReader.connect();
-            rfidReader.setPower(Integer.valueOf(readerProperties
-                    .getProperty("reader.power")));
-        } catch (IOException ex) {
-        }
-        
-        String[] test;
-        try {
-            test = rfidReader.readEPC();
-            System.out.println(test.length);
-            for (int i = 0; i < test.length; i++) {
-                identification=test[i];
-            }
-        } catch (IOException e) {
+            jso = JSObject.getWindow(this);
+        } catch (JSException e) {
             // TODO Auto-generated catch block
-            e.printStackTrace();
+            toDraw="error jsobject";
         }
+        
         
         repaint();
     }
@@ -54,8 +53,46 @@ public class RFIDAccess extends Applet {
     { 
 //method to draw text on screen 
 // String first, then x and y coordinate. 
-     g.drawString("Hey "+ identification,20,20); 
+     g.drawString("Hey "+ toDraw,20,20); 
 
+    }
+    
+    public void read() {
+        String[] test;
+        try {
+            test = rfidReader.readEPC();
+            System.out.println(test.length);
+            for (int i = 0; i < test.length; i++) {
+                toDraw=test[i];
+                idRead=toDraw;
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        repaint();
+        
+        printResult();
+    }
+    
+    public void connect() {
+        try {
+            rfidReader.connect();
+            rfidReader.setPower(Integer.valueOf(readerProperties
+                    .getProperty("reader.power")));
+        } catch (IOException ex) {
+            
+        }
+    }
+    
+    public void printResult() {
+        try {
+            jso.call("printResult", new String[] {String.valueOf(idRead)});
+        }
+        catch (Exception ex) {
+                ex.printStackTrace();
+        }
     }
     
     
